165f5dad0c30ac0dec90dba504ea3f094b0126ea
Michael Sawitzki <m.sawitzki99@gmail.com>
2023-12-13 22:38:44 +0100

    Changed the method for outliers

--- create_holdout.py
+++ create_holdout.py
@@ -6,17 +6,12 @@ import numpy as np
 train = pd.read_csv("Train.csv")
 print(f"The old length of the Dataset: {len(train)}")
 
-# divide the 19 outliers by 10
-a = 6000
-b = 500
+# divide the 21 outliers by 10
 
-def line(x):
-    return a * x + b
-
-over_line_df = train[(train['Yield'] >= train['Acre'].apply(line)) & (train['Yield'] > 2500)]
-print(f"Number of outliers: {len(over_line_df)}")
-mult_by_10_index = over_line_df.index
-train.loc[mult_by_10_index, 'Yield'] = over_line_df.Yield / 10
+outliers = train[((train.Yield / train.Acre) >= 6500)]
+print(f"Number of outliers: {len(outliers)}")
+mult_by_10_index = outliers.index
+train.loc[mult_by_10_index, 'Yield'] = outliers.Yield / 10
 
 # sample random ids and create holdout set
 np.random.seed(123)
@@ -26,7 +21,16 @@ train_holdout = train.loc[rand_idx]
 train_new = train.drop(rand_idx)
 print(f"length of the new train set: {len(train_new)}"), print(f"length of the holdout set: {len(train_holdout)}")
 
-# create the new csv files
+print("Distribution of District in old train set:") 
+print(f"{train['District'].value_counts()/len(train)}")
+print("Distribution of District in new train set:") 
+print(f"{train_new['District'].value_counts()/len(train_new)}")
+print("Distribution of District in new test set:") 
+print(f"{train_holdout['District'].value_counts()/len(train_holdout)}")
 
+# create the new csv files
 train_new.to_csv('train_new.csv', index=False)
 train_holdout.to_csv('train_holdout.csv', index=False)
+
+
+
